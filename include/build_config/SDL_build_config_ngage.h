b9b67f771defbea10a38010d68df7675c6588a2d
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-12-28 09:29:14 +0100

    Add additional platform-specific build options for the Nokia N-Gage.

--- include/build_config/SDL_build_config_ngage.h
+++ include/build_config/SDL_build_config_ngage.h
@@ -23,8 +23,6 @@
 #define SDL_build_config_ngage_h_
 #define SDL_build_config_h_
 
-#define IMPORT_C extern
-
 #include <SDL3/SDL_platform_defines.h>
 
 typedef signed char int8_t;
@@ -39,5 +37,20 @@ typedef unsigned long uintptr_t;
 typedef signed long intptr_t;
 
 #define SDL_THREADS_DISABLED 1
+#define HAVE_CEIL            0
+#define HAVE_COPYSIGN        0
+#define HAVE_CO              0
+#define HAVE_EXP             0
+#define HAVE_FABS            0
+#define HAVE_FLOOR           0
+#define HAVE_LOG             0
+#define HAVE_LOG10           0
+#define HAVE_MALLOC          0
+#define HAVE_MATH_H          0
+#define HAVE_SCALBN          0
+#define HAVE_SIN             0
+#define HAVE_SQRT            0
+#define HAVE_TAN             0
+#define LACKS_SYS_MMAN_H     1
 
 #endif /* SDL_build_config_ngage_h_ */
